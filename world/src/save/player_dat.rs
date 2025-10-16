4a0e19c9ada73d7c2576f7e795ac652b1be98d53
Vidar Magnusson <git@vidarmagnusson.com>
2025-10-17 00:43:12 +0200

    Update player_dat.rs

--- world/src/save/player_dat.rs
+++ world/src/save/player_dat.rs
@@ -1,7 +1,6 @@
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 pub struct PlayerDat {
     /* Common to all entities */
     #[serde(flatten)]
@@ -9,74 +8,95 @@ pub struct PlayerDat {
     /* Common to all mobs */
     #[serde(flatten)]
     mob_tags: Mob,
+    #[serde(rename = "abilities")]
     abilities: Abilities,
+    #[serde(rename = "DataVersion")]
     data_version: i32, // Increases for each snapshot & release.
+    #[serde(rename = "Dimension")]
     dimension: String,
     // TODO: EnderItems, confusing.
     #[serde(rename = "enteredNeatherPosition")]
     entered_neather_position: Option<NeatherPosition>,
+    #[serde(rename = "foodExhaustionLevel")]
     food_exhaustion_level: f32,
+    #[serde(rename = "foodLevel")]
     food_level: i32,
+    #[serde(rename = "foodSaturationLevel")]
     food_saturation_level: f32,
+    #[serde(rename = "foodTickTimer")]
     food_tick_timer: i32,
     // TODO: Inventory
     last_death_location: Option<DeathLocation>,
     #[serde(rename = "playerGameType")]
     player_game_type: i32,
-    previous_player_game_type: i32,
+    previous_player_game_type: Option<i32>,
     // TODO: recipeBook
     // TODO: RootVehicle
+    #[serde(rename = "Score")]
     score: i32,
+    #[serde(rename = "seenCredits")]
     seen_credits: bool,
     // Not stored in level.dat
     selected_item: Option<Item>,
+    #[serde(rename = "SelectedItemSlot")]
     selected_item_slot: i32,
     shoulder_entity_left: Option<Entity>,
     shoulder_entity_right: Option<Entity>,
+    #[serde(rename = "SleepTimer")]
     sleep_timer: i16,
-    spawn_dimension: String,
-    spawn_forced: bool,
+    spawn_dimension: Option<String>,
+    spawn_forced: Option<bool>,
     spawn_x: Option<i32>,
     spawn_y: Option<i32>,
     spawn_z: Option<i32>,
     warden_spawn_tracker: WardenSpawnTracker,
+    #[serde(rename = "XpLevel")]
     xp_level: i32,
     #[serde(rename = "XpP")]
     xp_percent: f32,
+    #[serde(rename = "XpSeed")]
     xp_seed: i32,
+    #[serde(rename = "XpTotal")]
     xp_total: i32,
 }
 
 /// Tags common between all entities.
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct Entity {
+    #[serde(rename = "Air")]
     air: i16,
-    custom_name: String,
-    custom_name_visible: bool,
+    custom_name: Option<String>,
+    custom_name_visible: Option<bool>,
     fall_distance: f32,
+    #[serde(rename = "Fire")]
     fire: i16,
-    glowing: bool,
-    has_visual_fire: bool,
-    id: String,
+    glowing: Option<bool>,
+    has_visual_fire: Option<bool>,
+    id: Option<String>,
+    #[serde(rename = "Invulnerable")]
     invulnerable: bool,
+    #[serde(rename = "Motion")]
     motion: [f64; 3], // Always contains 3 elements?
-    no_gravity: bool,
+    no_gravity: Option<bool>,
+    #[serde(rename = "OnGround")]
     on_ground: bool,
     // TODO: Instead of using EntityTags here we should probably have some big enum of all entities.
-    passengers: Vec<Entity>,
+    passengers: Option<Vec<Entity>>,
+    #[serde(rename = "PortalCooldown")]
     portal_cooldown: i32,
-    pos: [f64; 3],      // Always contains 3 elements?
+    #[serde(rename = "Pos")]
+    pos: [f64; 3], // Always contains 3 elements?
+    #[serde(rename = "Rotation")]
     rotation: [f32; 2], // Always contains 2 elements?
-    silent: bool,
-    tags: Vec<String>,
+    silent: Option<bool>,
+    tags: Option<Vec<String>>,
     ticks_frozen: Option<i32>,
+    #[serde(rename = "UUID")]
     uuid: [i32; 4], // TODO: Is really a UUID & always has length 4
 }
 
 /// Tags common between all entities.
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct Mob {
     // TODO: From wiki but "Tags for all mobs, except HandItems, ArmorItems, HandDropChances, ArmorDropChances, CanPickUpLoot, PersistenceRequired and Leash"
     // Unclear...
@@ -109,7 +129,6 @@ struct Mob {
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct PotionEffect {
     ambient: bool,
     amplifier: bool,
@@ -123,14 +142,12 @@ struct PotionEffect {
 /// Tags common for all Items.
 // TODO: tag, slot
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct Item {
     count: i8,
     id: Option<String>, // If not specified => change to stone?
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct Attribute {
     name: String,
     base: f64,
@@ -138,7 +155,6 @@ struct Attribute {
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct Modifier {
     amount: f64,
     name: String,
@@ -147,7 +163,6 @@ struct Modifier {
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
 struct Brain {
     // TODO: Memories: "Empty for all but allays, axolotls, frogs, goats, piglins, villagers, and wardens"
     #[serde(rename = "memories")]
@@ -177,6 +192,7 @@ struct Abilities {
     instabuild: bool,
     invulnerable: bool,
     may_build: bool,
+    #[serde(rename = "mayfly")]
     may_fly: bool,
     walk_speed: f32, // Always 0.1
 }
@@ -199,4 +215,30 @@ struct WardenSpawnTracker {
     cooldown_ticks: i32,
     ticks_since_last_warning: i32,
     warning_level: i32,
+}
+
+#[cfg(test)]
+mod test {
+    use std::path::Path;
+
+    use nbt::{read_nbt_file, ser::deserializer::Deserializer};
+    use serde::Deserialize;
+
+    use crate::save::player_dat::PlayerDat;
+
+    #[test]
+    fn parse_player_dat_file() {
+        let Some(nbt) = read_nbt_file(Path::new(
+            "../test-data/test-world/playerdata/df61b106-ca06-46b5-958a-99923cfe7ba6.dat",
+        ))
+        .expect("Failed to read player file") else {
+            panic!("Failed")
+        };
+
+        let deserializer = Deserializer::from_nbt_tag(nbt.payload);
+        let parsed = PlayerDat::deserialize(deserializer).expect("Failed to deserialize file");
+        println!("{parsed:?}");
+
+        todo!()
+    }
 }
