8332bacc8a1b5140ecfca6e0c2bd417246189536
Vidar Magnusson <git@vidarmagnusson.com>
2025-10-29 09:16:13 +0100

    Implement MAX_SIZE validation for json strings.

--- coms/src/codec/json_string.rs
+++ coms/src/codec/json_string.rs
@@ -20,8 +20,14 @@ impl<T: Serialize, const MAX_SIZE: usize> Serialize for JsonString<T, MAX_SIZE> 
     where
         S: serde::Serializer,
     {
-        // TODO: Validate length using MAX_SIZE.
         let json_string = serde_json::to_string(&self.inner).map_err(serde::ser::Error::custom)?;
+
+        if json_string.len() > MAX_SIZE {
+            return Err(serde::ser::Error::custom(format!(
+                "Invalid length for JsonString, expected max_length: {MAX_SIZE}, got length: {}",
+                json_string.len()
+            )));
+        }
 
         json_string.serialize(serializer)
     }
@@ -43,7 +49,13 @@ impl<'de, T: DeserializeOwned, const MAX_SIZE: usize> Deserialize<'de> for JsonS
         // TODO: Maybe a bit of unnecessary allocations here, also only Owned supported atm.
         let s = String::deserialize(deserializer)?;
 
-        // TODO: Validate size.
+        if s.len() > MAX_SIZE {
+            return Err(serde::de::Error::custom(format!(
+                "JsonString longer than max allowed value for this field, max {MAX_SIZE}, got {}",
+                s.len()
+            )));
+        }
+
         let t: T = serde_json::from_str(&s).map_err(serde::de::Error::custom)?;
 
         Ok(Self { inner: t })
