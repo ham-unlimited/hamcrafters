7d01077313dc41f653779d37e0c542fd1b780251
Vidar Magnusson <git@vidarmagnusson.com>
2025-11-09 16:47:54 +0100

    Fix clippy warnings

--- client-handler/src/client_data.rs
+++
@@ -1,2 +0,0 @@
-/// Information about a specific client, setup during config & gameplay.
-pub struct ClientData {} // TODO: Store config information.
--- client-handler/src/lib.rs
+++ client-handler/src/lib.rs
@@ -8,6 +8,3 @@ pub mod client_handler;
 
 /// Errors that can occurr when communicating with a client.
 pub mod client_error;
-
-/// Information about the state of this client set up during config and gameplay.
-pub mod client_data;
