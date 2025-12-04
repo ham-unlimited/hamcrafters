ac4c43e1827e2e7392f08afa98f766318719aa41
Vidar Magnusson <git@vidarmagnusson.com>
2025-12-04 22:23:53 +0100

    Fix formatting

--- nbt/src/nbt_value/value.rs
+++ nbt/src/nbt_value/value.rs
@@ -22,37 +22,41 @@ pub enum NbtValue {
 
 impl Display for NbtValue {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            NbtValue::Byte(b) => b.to_string(),
-            NbtValue::Short(s) => s.to_string(),
-            NbtValue::Int(i) => i.to_string(),
-            NbtValue::Long(l) => l.to_string(),
-            NbtValue::Float(f) => f.to_string(),
-            NbtValue::Double(d) => d.to_string(),
-            NbtValue::ByteArray(bytes) => {
-                let bs = bytes.iter().map(|b| b.to_string()).collect::<Vec<_>>();
-                format!("[{}]", bs.join(", "))
+        write!(
+            f,
+            "{}",
+            match self {
+                NbtValue::Byte(b) => b.to_string(),
+                NbtValue::Short(s) => s.to_string(),
+                NbtValue::Int(i) => i.to_string(),
+                NbtValue::Long(l) => l.to_string(),
+                NbtValue::Float(f) => f.to_string(),
+                NbtValue::Double(d) => d.to_string(),
+                NbtValue::ByteArray(bytes) => {
+                    let bs = bytes.iter().map(|b| b.to_string()).collect::<Vec<_>>();
+                    format!("[{}]", bs.join(", "))
+                }
+                NbtValue::String(s) => format!("\"{s}\""),
+                NbtValue::List(nbt_values) => {
+                    let vs = nbt_values.iter().map(|v| v.to_string()).collect::<Vec<_>>();
+                    format!("[{}]", vs.join(", "))
+                }
+                NbtValue::Compound(hash_map) => {
+                    let vs = hash_map
+                        .iter()
+                        .map(|(a, b)| format!("\"{a}\": {}", b))
+                        .collect::<Vec<_>>();
+                    format!("{{{}}}", vs.join(", "))
+                }
+                NbtValue::IntArray(items) => {
+                    let is = items.iter().map(|i| i.to_string()).collect::<Vec<_>>();
+                    format!("[{}]", is.join(", "))
+                }
+                NbtValue::LongArray(items) => {
+                    let ls = items.iter().map(|l| l.to_string()).collect::<Vec<_>>();
+                    format!("[{}]", ls.join(", "))
+                }
             }
-            NbtValue::String(s) => format!("\"{s}\""),
-            NbtValue::List(nbt_values) => {
-                let vs = nbt_values.iter().map(|v| v.to_string()).collect::<Vec<_>>();
-                format!("[{}]", vs.join(", "))
-            }
-            NbtValue::Compound(hash_map) => {
-                let vs = hash_map
-                    .iter()
-                    .map(|(a, b)| format!("\"{a}\": {}", b))
-                    .collect::<Vec<_>>();
-                format!("{{{}}}", vs.join(", "))
-            }
-            NbtValue::IntArray(items) => {
-                let is = items.iter().map(|i| i.to_string()).collect::<Vec<_>>();
-                format!("[{}]", is.join(", "))
-            }
-            NbtValue::LongArray(items) => {
-                let ls = items.iter().map(|l| l.to_string()).collect::<Vec<_>>();
-                format!("[{}]", ls.join(", "))
-            }
-        })
+        )
     }
 }
