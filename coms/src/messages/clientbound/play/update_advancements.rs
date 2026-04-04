995cd673e9be15d18cc5348cb485b6babac095c9
Vidar Magnusson <git@vidarmagnusson.com>
2026-04-04 02:24:46 +0200

    Update advancements packet fixes?

--- coms/src/messages/clientbound/play/update_advancements.rs
+++ coms/src/messages/clientbound/play/update_advancements.rs
@@ -1,7 +1,7 @@
 use crate::{
     McPacket,
     codec::{
-        identifier::Identifier, prefixed_array::PrefixedArray, prefixed_optional::PrefixedOptional,
+        identifier::Identifier, prefixed_array::PrefixedArray,
         var_int::VarInt,
     },
     messages::models::{slot::Slot, text_component::TextComponent},
@@ -41,13 +41,13 @@ pub struct AdvancementMapping {
 #[derive(Debug, Deserialize)]
 pub struct Advancement {
     /// The identifier of the parent advancement.
-    parent_id: PrefixedOptional<Identifier>,
+    parent_id: Option<Identifier>,
     /// Display data for the advancement.
-    display_data: PrefixedOptional<AdvancementDisplay>,
+    display_data: Option<AdvancementDisplay>,
     /// Array with a sub-array of criteria. To check if the requirements are met, each sub-array must be tested and mapped with the OR operator, resulting in a boolean array.
     /// These booleans must be mapped with the AND operator to get the result.\
     /// Strings have a max-size of 32767 characters.
-    nested_requirements: PrefixedArray<String>,
+    nested_requirements: PrefixedArray<PrefixedArray<String>>,
     /// Whether the client should include this achievement in the telemetry data when it's completed.
     /// The vanilla client only sends data for advancements on the minecraft namespace.
     sends_telemetry_data: bool,
@@ -119,6 +119,7 @@ impl<'de> Visitor<'de> for AdvancementDisplayVisitor {
         let x_coord = seq
             .next_element()?
             .ok_or(de::Error::invalid_length(6, &self))?;
+
         let y_coord = seq
             .next_element()?
             .ok_or(de::Error::invalid_length(7, &self))?;
@@ -161,15 +162,15 @@ impl<'de> Deserialize<'de> for AdvancementFrameType {
     where
         D: serde::Deserializer<'de>,
     {
-        let frame_type: u8 = u8::deserialize(deserializer)?;
+        let frame_type: VarInt = VarInt::deserialize(deserializer)?;
 
-        match frame_type {
+        match frame_type.0 {
             0 => Ok(Self::Task),
-            1 => Ok(Self::Goal),
-            2 => Ok(Self::Challenge),
+            1 => Ok(Self::Challenge),
+            2 => Ok(Self::Goal),
             _ => Err(serde::de::Error::custom(format!(
                 "Invalid advancement frame type: {}",
-                frame_type
+                frame_type.0
             ))),
         }
     }
@@ -213,9 +214,57 @@ pub struct AdvancementProgressMapping {
 /// Advancement progress for an advancement, such as which criteria have been completed and when.
 #[derive(Debug, Deserialize)]
 pub struct AdvancementProgress {
+    /// Mapping of criterion identifier to criterion progress.
+    criteria: PrefixedArray<CriterionProgress>,
+}
+
+/// Criterion progress for a single criterion.
+#[derive(Debug, Deserialize)]
+pub struct CriterionProgress {
     /// The identifier of the criterion.
     criterion_identifier: Identifier,
     /// Present if achieved. As returned by Date.getTime.
     // TODO: It is a long but it should really be a date type.
-    date_of_achieving: PrefixedOptional<i64>,
+    date_of_achieving: Option<i64>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::UpdateAdvancements;
+    use crate::ser::deserializer::Deserializer;
+    use serde::Deserialize;
+    use std::io::Cursor;
+
+    #[test]
+    fn deserializes_known_update_advancements_payload() {
+        let data: Vec<u8> = vec![
+            0x01, 0x02, 0x2c, 0x6d, 0x69, 0x6e, 0x65, 0x63, 0x72, 0x61, 0x66, 0x74, 0x3a, 0x72,
+            0x65, 0x63, 0x69, 0x70, 0x65, 0x73, 0x2f, 0x64, 0x65, 0x63, 0x6f, 0x72, 0x61, 0x74,
+            0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x63, 0x72, 0x61, 0x66, 0x74, 0x69, 0x6e, 0x67, 0x5f,
+            0x74, 0x61, 0x62, 0x6c, 0x65, 0x01, 0x16, 0x6d, 0x69, 0x6e, 0x65, 0x63, 0x72, 0x61,
+            0x66, 0x74, 0x3a, 0x72, 0x65, 0x63, 0x69, 0x70, 0x65, 0x73, 0x2f, 0x72, 0x6f, 0x6f,
+            0x74, 0x00, 0x01, 0x02, 0x0e, 0x68, 0x61, 0x73, 0x5f, 0x74, 0x68, 0x65, 0x5f, 0x72,
+            0x65, 0x63, 0x69, 0x70, 0x65, 0x11, 0x75, 0x6e, 0x6c, 0x6f, 0x63, 0x6b, 0x5f, 0x72,
+            0x69, 0x67, 0x68, 0x74, 0x5f, 0x61, 0x77, 0x61, 0x79, 0x00, 0x16, 0x6d, 0x69, 0x6e,
+            0x65, 0x63, 0x72, 0x61, 0x66, 0x74, 0x3a, 0x72, 0x65, 0x63, 0x69, 0x70, 0x65, 0x73,
+            0x2f, 0x72, 0x6f, 0x6f, 0x74, 0x00, 0x00, 0x01, 0x01, 0x0a, 0x69, 0x6d, 0x70, 0x6f,
+            0x73, 0x73, 0x69, 0x62, 0x6c, 0x65, 0x00, 0x00, 0x02, 0x2c, 0x6d, 0x69, 0x6e, 0x65,
+            0x63, 0x72, 0x61, 0x66, 0x74, 0x3a, 0x72, 0x65, 0x63, 0x69, 0x70, 0x65, 0x73, 0x2f,
+            0x64, 0x65, 0x63, 0x6f, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x63, 0x72,
+            0x61, 0x66, 0x74, 0x69, 0x6e, 0x67, 0x5f, 0x74, 0x61, 0x62, 0x6c, 0x65, 0x02, 0x0e,
+            0x68, 0x61, 0x73, 0x5f, 0x74, 0x68, 0x65, 0x5f, 0x72, 0x65, 0x63, 0x69, 0x70, 0x65,
+            0x00, 0x11, 0x75, 0x6e, 0x6c, 0x6f, 0x63, 0x6b, 0x5f, 0x72, 0x69, 0x67, 0x68, 0x74,
+            0x5f, 0x61, 0x77, 0x61, 0x79, 0x01, 0x00, 0x00, 0x01, 0x9d, 0x3b, 0xba, 0x14, 0xc0,
+            0x16, 0x6d, 0x69, 0x6e, 0x65, 0x63, 0x72, 0x61, 0x66, 0x74, 0x3a, 0x72, 0x65, 0x63,
+            0x69, 0x70, 0x65, 0x73, 0x2f, 0x72, 0x6f, 0x6f, 0x74, 0x01, 0x0a, 0x69, 0x6d, 0x70,
+            0x6f, 0x73, 0x73, 0x69, 0x62, 0x6c, 0x65, 0x00, 0x01,
+        ];
+
+        let mut de = Deserializer::new(Cursor::new(data));
+        let parsed = UpdateAdvancements::deserialize(&mut de);
+        assert!(
+            parsed.is_ok(),
+            "failed to parse update advancements: {parsed:?}"
+        );
+    }
 }
